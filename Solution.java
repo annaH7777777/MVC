62a080cad9441596651e0dfb67fe55e54de590fb
annaH7777777 <49167607+annaH7777777@users.noreply.github.com>
2019-05-11 13:24:38 +0300

    Initial commit

---
+++ Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task36.task3608;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.MainModel;
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
+import com.javarush.task.task36.task3608.view.UsersView;
+
+public class Solution {
+    public static void main(String[] args) {
+        Model model = new MainModel();
+        UsersView usersView = new UsersView();
+        Controller controller = new Controller();
+
+        usersView.setController(controller);
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+
+        usersView.fireEventShowAllUsers();
+
+        EditUserView editUserView = new EditUserView();
+        controller.setEditUserView(editUserView);
+        usersView.fireEventOpenUserEditForm(126);
+
+        editUserView.setController(controller);
+        editUserView.fireEventUserDeleted(124);
+
+        editUserView.fireEventUserChanged("a", 126, 3);
+
+        usersView.fireEventShowDeletedUsers();
+    }
+}
